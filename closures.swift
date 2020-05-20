3e8c889472ffdc7d702015c8184bf9ce518f92d4
mohammedibrahim-mi <34709179+mohammedibrahim-mi@users.noreply.github.com>
2020-05-20 18:18:15 +0530

    Create closures.swift

---
+++ closures.swift
@@ -0,0 +1,21 @@
+//closure program 1
+
+var str = "Hello"
+
+let heroes = ["Hulk", "Spiderman", "Iron Man", "Captain America"]
+
+func sorty(_ s1: String,  _ s2: String) -> Bool{
+    return s1 < s2
+}
+print(heroes)
+
+//var newHeroes = heroes.sorted(by: sorty)
+var newHeroes = heroes.sorted(by: {
+    (_ s1: String,  _ s2: String) -> Bool in
+    return s1 < s2
+})
+
+print(newHeroes)
+
+
+//program 2
