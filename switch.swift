f5dc615f9fd2c51cf7ba93855d69e9dec87080dd
mohammedibrahim-mi <34709179+mohammedibrahim-mi@users.noreply.github.com>
2020-05-20 18:24:21 +0530

    Create switch.swift

---
+++ switch.swift
@@ -0,0 +1,18 @@
+let userRated: Int = 6
+
+switch userRated {
+case 1...5:
+    print("Thanks for giving 5 star to us !")
+    
+case 4:
+    print("Thanks for giving 4 star to us !")
+case 3:
+  print("Thanks for giving 3 star to us !")
+case 2:
+    print("Thanks for giving 2 star to us !")
+case 1:
+    print("Thanks for giving 1 star to us !")
+default:
+    print("Please rate between 1 and 5")
+}
+
