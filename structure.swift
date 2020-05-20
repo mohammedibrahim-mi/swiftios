a916b5cc196dfc628b44bf064f2e24b050c3f272
mohammedibrahim-mi <34709179+mohammedibrahim-mi@users.noreply.github.com>
2020-05-20 18:40:28 +0530

    Create structure.swift

---
+++ structure.swift
@@ -0,0 +1,27 @@
+//struct function
+
+struct Resolution {
+ var width = 0 
+var height = 0 
+} 
+class VideoMode
+ {
+ var resolution = Resolution()
+ var interlaced = false
+ var frameRate = 0.0
+ var name: String? 
+}
+Struct Res {
+var w=0
+var v=0
+}
+Class Video
+{
+var res= Res()
+var inter = false
+var frame 0.0
+var title : string ?
+}
+let some = Res() [‘some’ is instance of Res] let oneSome = video()
+some.h
+oneSome.res.h
